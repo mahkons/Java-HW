263a633764ee00e165e5c17b3ecba13d3a1b0d71
Makhnev Konstantin <mah.kons@gmail.com>
2019-02-01 20:46:06 +0300

    HW-3 initial commit

---
+++ HW-3/src/main/java/ru/hse/kostya/java/SimpleMyTreeSet.java
@@ -0,0 +1,58 @@
+package ru.hse.kostya.java;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+
+public class SimpleMyTreeSet<E> extends AbstractSet<E> implements MyTreeSet<E> {
+
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public MyTreeSet<E> descendingSet() {
+        return null;
+    }
+
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E last() {
+        return null;
+    }
+
+    @Override
+    public E lower(E e) {
+        return null;
+    }
+
+    @Override
+    public E floor(E e) {
+        return null;
+    }
+
+    @Override
+    public E ceiling(E e) {
+        return null;
+    }
+
+    @Override
+    public E higher(E e) {
+        return null;
+    }
+}
