ffd122685a92e6fc656fc5a81d9bd9d9382bd255
Makhnev Konstantin <mah.kons@gmail.com>
2019-02-04 21:40:58 +0300

    Tests for iterators added and passed

--- HW-3/src/test/java/ru/hse/kostya/java/SimpleMyTreeSetTest.java
+++ HW-3/src/test/java/ru/hse/kostya/java/SimpleMyTreeSetTest.java
@@ -3,6 +3,9 @@ package ru.hse.kostya.java;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleMyTreeSetTest {
@@ -18,7 +21,7 @@ class SimpleMyTreeSetTest {
         simpleSetNoComparator.add("BBB");
 
         simpleSetWithComparator.add("aaa");
-        simpleSetWithComparator.add("AAA");
+        simpleSetWithComparator.add("ccc");
         simpleSetWithComparator.add("BBB");
     }
 
@@ -30,14 +33,48 @@ class SimpleMyTreeSetTest {
 
     @Test
     void iterator() {
+        StringBuilder summary = new StringBuilder();
+        for (String elem : simpleSetNoComparator) {
+            summary.append(elem);
+        }
+        assertEquals("AAABBBaaa", summary.toString());
+
+        Iterator<String> setIterator = simpleSetNoComparator.iterator();
+        assertEquals("AAA", setIterator.next());
+
+        assertThrows(ConcurrentModificationException.class, () -> {
+           simpleSetNoComparator.add("ccc");
+           assertTrue(setIterator.hasNext());
+        });
+
+        Iterator<String> setIteratorToEnd = simpleSetWithComparator.iterator();
+        for (int i = 0; i < simpleSetWithComparator.size(); i++) {
+            setIteratorToEnd.next();
+        }
+        assertFalse(setIteratorToEnd.hasNext());
     }
 
     @Test
     void descendingIterator() {
+        Iterator<String> setDescendingIterator = simpleSetNoComparator.descendingIterator();
+        assertEquals("aaa", setDescendingIterator.next());
+        assertEquals("BBB", setDescendingIterator.next());
+        assertEquals("AAA", setDescendingIterator.next());
+        assertFalse(setDescendingIterator.hasNext());
     }
 
     @Test
     void descendingSet() {
+        MyTreeSet<String> descendingTreeSet = simpleSetNoComparator.descendingSet();
+
+        StringBuilder summary = new StringBuilder();
+        for (String elem : descendingTreeSet) {
+            summary.append(elem);
+        }
+        assertEquals("aaaBBBAAA", summary.toString());
+
+        assertEquals("aaa", descendingTreeSet.lower("BBB"));
+        assertEquals("BBB", descendingTreeSet.ceiling("BBB"));
     }
 
     @Test
@@ -80,6 +117,8 @@ class SimpleMyTreeSetTest {
         assertEquals("AAA", simpleSetNoComparator.lower("BBB"));
         assertNull(simpleSetNoComparator.lower("AAA"));
         assertEquals("aaa", simpleSetNoComparator.lower("zzz"));
+
+        assertEquals("BBB", simpleSetWithComparator.lower("ccc"));
     }
 
     @Test
@@ -87,6 +126,8 @@ class SimpleMyTreeSetTest {
         assertEquals("BBB", simpleSetNoComparator.floor("BBB"));
         assertNull(simpleSetNoComparator.floor("A"));
         assertEquals("aaa", simpleSetNoComparator.floor("zzz"));
+
+        assertEquals("ccc", simpleSetWithComparator.floor("ccc"));
     }
 
     @Test
@@ -94,6 +135,8 @@ class SimpleMyTreeSetTest {
         assertEquals("aaa", simpleSetNoComparator.higher("BBB"));
         assertNull(simpleSetNoComparator.higher("aaa"));
         assertEquals("AAA", simpleSetNoComparator.higher("A"));
+
+        assertEquals("BBB", simpleSetWithComparator.higher("aaa"));
     }
 
     @Test
@@ -101,5 +144,7 @@ class SimpleMyTreeSetTest {
         assertEquals("BBB", simpleSetNoComparator.ceiling("BBB"));
         assertNull(simpleSetNoComparator.ceiling("zzz"));
         assertEquals("AAA", simpleSetNoComparator.ceiling("A"));
+
+        assertEquals("ccc", simpleSetWithComparator.ceiling("ccc"));
     }
 }
