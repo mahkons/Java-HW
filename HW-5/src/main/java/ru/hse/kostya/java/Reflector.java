53eb3852f911625b3a1c7dff8adfe50229bfa761
Makhnev Konstantin <mah.kons@gmail.com>
2019-03-05 02:07:27 +0300

    Reflector implemented, javadocs added and template for tests added

--- HW-5/src/main/java/ru/hse/kostya/java/Reflector.java
+++ HW-5/src/main/java/ru/hse/kostya/java/Reflector.java
@@ -1,80 +1,125 @@
 package ru.hse.kostya.java;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.lang.reflect.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Utility class that contains functions able to print class structure
+ *      and compare contents of classes.
+ */
 public class Reflector {
 
     private static final String indent = "    "; // four spaces
-    private static final String unsupportedOperation = " {\n" +
-            indent + indent + "throw new UnsupportedOperationException();\n" +
-            indent + "}\n";
+    private static final String linebreak = System.lineSeparator();
+    private static final String unsupportedOperation = " {" + linebreak +
+            indent + indent + "throw new UnsupportedOperationException();" + linebreak +
+            indent + "}" + linebreak;
 
-    public static void printStructure(Class<?> someClass) throws IOException {
+    /**
+     * Writes class structure in a new file in current directory.
+     * Name of new file equals to class simple name
+     *
+     * Writes class constructors, methods, fields, nested and inner classes
+     *      declared in that class
+     *
+     * @throws IOException if any problem occurred during writing to file previously named
+     */
+    public static void printStructure(@NotNull Class<?> someClass) throws IOException {
         try (var writer = new FileWriter(someClass.getSimpleName() + ".java")) {
             printClass(writer, someClass);
         }
     }
 
-    private static void printClass(FileWriter writer, Class<?> someClass) throws IOException {
+    /**
+     * Writes class with declaration and full content.
+     */
+    private static void printClass(Writer writer, Class<?> someClass) throws IOException {
         writeClassDeclaration(writer, someClass);
-        writer.write(" {\n\n");
+        writer.write(" {" + linebreak + linebreak);
         writeClassContent(writer, someClass);
-        writer.write("}\n");
+        writer.write("}" + linebreak);
     }
 
-    public static void diffClasses(Class<?> a, Class<?> b) {
-
-    }
-
-    private static void writeClassNameWithType(FileWriter writer, Class<?> someClass) throws IOException {
+    /**
+     * Writes class name with full List of type parameters.
+     */
+    private static void writeClassNameWithType(Writer writer, Class<?> someClass) throws IOException {
         writer.write(someClass.getSimpleName());
-        writeTypesArray(writer, someClass.getTypeParameters(), "<", ", ", ">");
+        writeTypesArray(writer, someClass.getTypeParameters(), "<", ", ", ">", true);
     }
 
-    private static void writeClassDeclaration(FileWriter writer, Class<?> someClass) throws IOException {
+    /**
+     * Writes class with modifiers, class keyWord, full name with parameters, superclass and implemented interfaces.
+     */
+    private static void writeClassDeclaration(Writer writer, Class<?> someClass) throws IOException {
         writer.write(Modifier.toString(someClass.getModifiers()));
         writer.write(" class ");
         writeClassNameWithType(writer, someClass);
 
         writer.write(" extends ");
-        writeType(writer,someClass.getGenericSuperclass());
-        writeTypesArray(writer, someClass.getGenericInterfaces(), " implements ", ", ", "");
+        writeType(writer,someClass.getGenericSuperclass(), true);
+        writeTypesArray(writer, someClass.getGenericInterfaces(), " implements ", ", ", "", true);
     }
 
-    private static void writeClassContent(FileWriter writer, Class<?> someClass) throws IOException {
+    /**
+     * Writes all fields, methods, constructors, inner and nested classes.
+     * Methods and constructors bodies are just {@code throw new UnsupportedOperationException statement}
+     * Fields initialized to there default value
+     */
+    private static void writeClassContent(Writer writer, Class<?> someClass) throws IOException {
 
         for (Class<?> clazz : someClass.getDeclaredClasses()) {
             printClass(writer, clazz);
-            writer.write("\n");
+            writer.write("" + linebreak);
         }
         for (Constructor<?> constructor : someClass.getDeclaredConstructors()) {
+            if (constructor.isSynthetic()) {
+                continue;
+            }
             writer.write(indent);
             writeConstructor(writer, constructor);
-            writer.write("\n");
+            writer.write(unsupportedOperation);
+            writer.write("" + linebreak);
         }
         for (Method method : someClass.getDeclaredMethods()) {
+            if (method.isSynthetic()) {
+                continue;
+            }
             writer.write(indent);
             writeMethod(writer, method);
-            writer.write("\n");
+            writer.write(unsupportedOperation);
+            writer.write("" + linebreak);
         }
         for (Field field : someClass.getDeclaredFields()) {
+            if (field.isSynthetic()) {
+                continue;
+            }
             writer.write(indent);
             writeField(writer, field);
-            writer.write("\n");
+            writer.write("" + linebreak);
         }
 
     }
 
+    /**
+     *  Removes {@code extends Object} cause it looks ugly.
+     */
     private static Type[] filterUpperBounds(Type[] typeParameters) {
         return Arrays.stream(typeParameters).filter(x -> !(Object.class.equals(x))).toArray(Type[]::new);
     }
 
-    private static void writeType(FileWriter writer, Type type) throws IOException {
+    /**
+     * Writes given class, TypeVariable, ParametrizedType, WildcardType or GenericType object.
+     * @param isDeclaration settles if TypeVariable should be written with it bounds
+     */
+    private static void writeType(Writer writer, Type type, boolean isDeclaration) throws IOException {
 
         if (type instanceof Class<?>) {
             var clazz = (Class<?>)type;
@@ -82,31 +127,43 @@ public class Reflector {
         } else if (type instanceof TypeVariable) {
             var typeVariable = (TypeVariable<?>)type;
             writer.write(typeVariable.getName());
-            writeTypesArray(writer, filterUpperBounds(typeVariable.getBounds()), " extends ", " & ", "");
+            if (isDeclaration) {
+                writeTypesArray(writer, filterUpperBounds(typeVariable.getBounds()),
+                        " extends ", " & ", "", false);
+            }
         } else if (type instanceof WildcardType) {
             var wildcardType = (WildcardType)type;
             writer.write("?");
-            writeTypesArray(writer, filterUpperBounds(wildcardType.getUpperBounds()), " extends ", " & ", "");
-            writeTypesArray(writer, wildcardType.getLowerBounds(), " super ", " & ", " ");
+            writeTypesArray(writer, filterUpperBounds(wildcardType.getUpperBounds()),
+                    " extends ", " & ", "", false);
+            writeTypesArray(writer, wildcardType.getLowerBounds(),
+                    " super ", " & ", " ", false);
         } else if (type instanceof ParameterizedType) {
             var parametrisedType = (ParameterizedType)type;
             writer.write(parametrisedType.getRawType().toString());
-            writeTypesArray(writer, parametrisedType.getActualTypeArguments(), "<", ", ", ">");
+            writeTypesArray(writer, parametrisedType.getActualTypeArguments(),
+                    "<", ", ", ">", false);
         } else if (type instanceof GenericArrayType) {
             var genericArrayType = (GenericArrayType)type;
-            writeType(writer, genericArrayType.getGenericComponentType());
+            writeType(writer, genericArrayType.getGenericComponentType(), isDeclaration);
             writer.write("[]");
         }
     }
 
-    private static void writeTypesArray(FileWriter writer, Type[] typeParameters,
-                                        String prefix, String separator, String suffix) throws IOException {
+    /**
+     *  Writes array of Types.
+     *  Uses prefix, separator and suffix to combine elements
+     *  @param isDeclaration settles if TypeVariable should be written with it bounds
+     */
+    private static void writeTypesArray(Writer writer, Type[] typeParameters,
+                                        String prefix, String separator, String suffix,
+                                        boolean isDeclaration) throws IOException {
         if (typeParameters.length == 0) {
             return;
         }
         writer.write(prefix);
         for (int i = 0; i < typeParameters.length; i++) {
-            writeType(writer, typeParameters[i]);
+            writeType(writer, typeParameters[i], isDeclaration);
             if (i != typeParameters.length - 1) {
                 writer.write(separator);
             }
@@ -114,41 +171,177 @@ public class Reflector {
         writer.write(suffix);
     }
 
-    private static void writeConstructor(FileWriter writer, Constructor<?> constructor) throws IOException {
-        if (constructor.isSynthetic()) {
-            return;
-        }
+    /**
+     * Writes constructor with modifiers, typeParameters and parameters.
+     */
+    private static void writeConstructor(Writer writer, Constructor<?> constructor) throws IOException {
         writer.write(Modifier.toString(constructor.getModifiers()) + " ");
+        writeTypesArray(writer, constructor.getTypeParameters(), "<", ", ", "> ", true);
         writer.write(constructor.getDeclaringClass().getSimpleName());
-        writeMethodOrConstructorParametersAndBody(writer, constructor.getGenericParameterTypes(), constructor.getGenericExceptionTypes());
+        writeMethodOrConstructorParameters(writer, constructor.getGenericParameterTypes(), constructor.getGenericExceptionTypes());
     }
 
-    private static void writeMethod(FileWriter writer, Method method) throws IOException {
-        if (method.isSynthetic()) {
-            return;
-        }
+    /**
+     * Writes constructor with modifiers, typeParameters, generic return type, name and parameters.
+     */
+    private static void writeMethod(Writer writer, Method method) throws IOException {
         writer.write(Modifier.toString(method.getModifiers()) + " ");
-        writeType(writer, method.getGenericReturnType());
+        writeTypesArray(writer, method.getTypeParameters(), "<", ", ", "> ", true);
+        writeType(writer, method.getGenericReturnType(), false);
         writer.write(" ");
         writer.write(method.getName());
-        writeMethodOrConstructorParametersAndBody(writer, method.getGenericParameterTypes(), method.getGenericExceptionTypes());
+        writeMethodOrConstructorParameters(writer, method.getGenericParameterTypes(), method.getGenericExceptionTypes());
     }
 
-    private static void writeMethodOrConstructorParametersAndBody(FileWriter writer, Type[] genericParameterTypes, Type[] genericExceptionTypes) throws IOException {
+    /**
+     * Writes parameters and exceptions.
+     * Names arguments as arg + positionAmongArguments
+     */
+    private static void writeMethodOrConstructorParameters(Writer writer, Type[] genericParameterTypes, Type[] genericExceptionTypes) throws IOException {
         writer.write("(");
-        writeTypesArray(writer, genericParameterTypes, "", ", ", "");
+        for (int i = 0; i < genericParameterTypes.length; i++) {
+            writeType(writer, genericParameterTypes[i], false);
+            writer.write(" arg" + i);
+            if (i + 1 != genericParameterTypes.length) {
+                writer.write(", ");
+            }
+        }
         writer.write(")");
-        writeTypesArray(writer, genericExceptionTypes, " throws ", ", ", "");
-        writer.write(unsupportedOperation);
+        writeTypesArray(writer, genericExceptionTypes, " throws ", ", ", "", false);
     }
 
-    private static void writeField(FileWriter writer, Field field) throws IOException {
-        if (field.isSynthetic()) {
-            return;
-        }
+    /**
+     * Writes field with modifiers and generic type.
+     * Initializes it to default value
+     */
+    private static void writeField(Writer writer, Field field) throws IOException {
         writer.write(Modifier.toString(field.getModifiers()) + " ");
-        writeType(writer, field.getGenericType());
-        writer.write(" " + field.getName() + ";\n");
+        writeType(writer, field.getGenericType(), false);
+        writer.write(" " + field.getName());
+        writer.write(" = " + getDefaultValue(field.getGenericType()) + ";" + linebreak);
+    }
+
+    /**
+     * Gets String representing default value for given type
+     *      as it would appear in source code
+     */
+    private static String getDefaultValue(Type type) {
+        if (type instanceof Class) {
+            var clazz = (Class<?>)type;
+            if (clazz.isPrimitive()) {
+                return Array.get(Array.newInstance(clazz, 1), 0).toString();
+            } else {
+                return "null";
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Prints to System.out all methods and fields which exists in exactly one class of the given class.
+     */
+    public static void diffClasses(@NotNull Class<?> first, @NotNull Class<?> second) throws IOException {
+        try(Writer writer = new FileWriter("DiffOutput.java")) {
+            writer.write("Writing methods and fields different in " + first.getSimpleName() + " and " + second.getSimpleName() + "" + linebreak);
+
+            List<Method> methodsOfFirstClassOnly = getDifferentMethods(first, second);
+            List<Method> methodsOfSecondClassOnly = getDifferentMethods(second, first);
+            if (methodsOfFirstClassOnly.isEmpty() && methodsOfSecondClassOnly.isEmpty()) {
+                writer.write("All methods are the same" + linebreak);
+            } else {
+                writer.write("Methods of " + first.getSimpleName() + " only:" + linebreak);
+                writeListOfMethods(first, writer, methodsOfFirstClassOnly);
+                writer.write("Methods of " + second.getSimpleName() + " only:" + linebreak);
+                writeListOfMethods(second, writer, methodsOfSecondClassOnly);
+            }
+
+            List<Field> fieldsOfFirstClassOnly = getDifferentFields(first, second);
+            List<Field> fieldsOfSecondClassOnly = getDifferentFields(second, first);
+            if (fieldsOfFirstClassOnly.isEmpty() && fieldsOfSecondClassOnly.isEmpty()) {
+                writer.write("All fields are the same" + linebreak);
+            } else {
+                writer.write("Fields of " + first.getSimpleName() + " only:" + linebreak);
+                WriteListOfFields(writer, first, fieldsOfFirstClassOnly);
+                writer.write("Fields of " + second.getSimpleName() + " only:" + linebreak);
+                WriteListOfFields(writer, second, fieldsOfSecondClassOnly);
+            }
+        }
+    }
+
+    private static void writeListOfMethods(Class<?> clazz, Writer writer, List<Method> methodsOfThisClassOnly) throws IOException {
+        for (Method method : methodsOfThisClassOnly) {
+            writeMethod(writer, method);
+        }
+        writer.write(linebreak);
+    }
+
+    private static void WriteListOfFields(Writer writer, Class<?> clazz, List<Field> fieldsOfThisClassOnly) throws IOException {
+        for (Field field : fieldsOfThisClassOnly) {
+            writeField(writer, field);
+        }
+        writer.write(linebreak);
+    }
+
+    /**
+     * Compares methods to equality.
+     * Uses there Modifiers, TypeParameters, ReturnType, ParameterTypes,
+     *  Exceptions and Names to check equivalence
+     */
+    private static boolean MethodsAreEqual(Method a, Method b) {
+        if (a.getModifiers() != b.getModifiers()) {
+            return false;
+        }
+        if (!Arrays.equals(a.getTypeParameters(), b.getTypeParameters())) {
+            return false;
+        }
+        if (!a.getGenericReturnType().equals(b.getGenericReturnType())) {
+            return false;
+        }
+        if (!Arrays.equals(a.getGenericParameterTypes(), b.getGenericParameterTypes())) {
+            return false;
+        }
+        if (!Arrays.equals(a.getGenericExceptionTypes(), b.getGenericExceptionTypes())) {
+            return false;
+        }
+        return a.getName().equals(b.getName());
+    }
+
+    /**
+     * Compares fields to equality.
+     * Uses there Modifiers, Types
+     *  and Names to check equivalence
+     */
+    private static boolean FieldsAreEqual(Field a, Field b) {
+        if (a.getModifiers() != b.getModifiers()) {
+            return false;
+        }
+        if (!a.getGenericType().equals(b.getGenericType())) {
+            return false;
+        }
+        return a.getName().equals(b.getName());
+    }
+
+    /**
+     * Return List off all Methods declared in first class,
+     *      which has no equal method among methods declared in second class.
+     * Equivalence checked according to MethodsAreEqual method
+     * Works in a time equals to the product of amount of methods in first and second class
+     */
+    private static List<Method> getDifferentMethods(Class<?> first, Class<?> second) {
+        return Arrays.stream(first.getDeclaredMethods()).
+                filter(x -> Arrays.stream(second.getDeclaredMethods()).noneMatch(y -> MethodsAreEqual(x, y))).collect(Collectors.toList());
+    }
+
+    /**
+     * Return List off all Fields declared in first class,
+     *      which has no equal field among fields declared in second class.
+     * Equivalence checked according to MethodsAreEqual method
+     * Works in a time equals to the product of amount of fields in first and second class
+     */
+    private static List<Field> getDifferentFields(Class<?> first, Class<?> second) {
+        return Arrays.stream(first.getDeclaredFields()).
+                filter(x -> Arrays.stream(second.getDeclaredFields()).noneMatch(y -> FieldsAreEqual(x, y))).collect(Collectors.toList());
     }
 
 }
--- HW-5/src/test/java/ru/hse/kostya/java/ReflectorTest.java
+++ HW-5/src/test/java/ru/hse/kostya/java/ReflectorTest.java
@@ -1,58 +1,123 @@
 package ru.hse.kostya.java;
 
+import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReflectorTest {
 
-    private static class JustClass<T> implements Serializable {
-
-        private void doIt(List<? super String> value) {
-            return;
-        }
-
-        public String str;
-        protected int[] a;
-        private T[] superType;
+    private static class SimplePrimitives {
+        int a;
+        byte b;
+        char c;
+        long d;
+        boolean e;
+        short f;
+        double g;
+        long h;
     }
 
-    protected static class SimpleClass<T extends String & List, S> extends AbstractList<S> implements Comparable<T>, Serializable {
+    private static class FieldsModifiers {
+        private int a;
+        protected static final long b = 0;
+    }
 
-        private final T PrivateFinalValue = null;
-        public static Object CONST = " ";
+    private static class methodModifiers {
+        private static int a() { return 0;}
+        final int result() { return 0; }
+    }
 
-        @Override
-        public int compareTo(@NotNull T o) {
+    private static class DefaultInitializingOfFinalFields {
+        final int a = 0;
+        final int[] b = null;
+        final char c = '0';
+        final String s = null;
+        final List<Integer> l = null;
+    }
+
+    private static class WithSuperClass extends Exception {
+        int a = 0;
+    }
+
+    private static class WithInterfaces implements Serializable, Comparable<Object> {
+        public int compareTo(Object o) {
             return 0;
         }
+    }
 
-        @Override
-        public S get(int index) {
-            return null;
+    private static class TestConstructors {
+        final int some;
+        private TestConstructors() { some = 0; }
+        public TestConstructors(int a) { some = 0; };
+        protected TestConstructors(String s, int some) { this.some = some; }
+    }
+
+    private static class TestMethods {
+        int superMethod() { return 0; };
+        int evenBetterOne(int arg) { return 0; }
+        private void hardOne(int arg, List<String> variable) {}
+    }
+
+    private static class GenericFields<T extends List<Object>, S> {
+        T a;
+        S[] b;
+        final List<S> l = null;
+    }
+
+    private static class GenericMethods<T, S> {
+        T simple() { return null; }
+        <E> T parametrized(E elem) { return null; }
+        <E extends T> void complexParameter(E elem) {}
+        <E extends List<S>, K> S hardOne(E elem) { return null; }
+        <E, K> S withComplexArg(List<S> l) { return null; }
+    }
+
+    private static class WildCardTypes<T, S> {
+        void simple(List<? super T> l) {}
+        Collection<? extends T> withReturnValue(){ return null; }
+        <E extends List<? super T>> T complexOne(Collection<? super Collection<? extends E>> l) { return null; }
+
+    }
+
+    static class NestedAndInnerClasses<T> {
+        static class Nested {
+            final int a = 0;
+            protected int f() {return 0; }
+
+            static class NestedInNested {
+                static int b = 0;
+                int otherFunction(int arg) {
+                    return 0;
+                }
+            }
         }
 
-        @Override
-        public int size() {
-            return 0;
+        public class InnerClass {
+            final int a = 0;
+            int f() { return 0; }
+
         }
     }
 
-    @Test
-    void SimpleClass() throws IOException {
-        Reflector.printStructure(SimpleClass.class);
+    //private static class BigAndComplexClass
+    //testing difference between classes
+    //tests themselves
+
+    private boolean writeToFileAndCompare(Class<?> clazz) throws IOException {
+        Reflector.printStructure(clazz);
+        var outputFile = new File(clazz.getSimpleName() + ".java");
+        var expectedFile = new File("src/test/resources/" + clazz.getSimpleName() + ".java");
+        return FileUtils.contentEquals(expectedFile, outputFile);
     }
 
-    @Test
-    void JustClass() throws IOException {
-        Reflector.printStructure(JustClass.class);
-    }
 
 
 }
