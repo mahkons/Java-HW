d2ac96681f5da0ae27c91ad6ae7bbc3860c89e5e
Makhnev Konstantin <mah.kons@gmail.com>
2019-04-26 15:38:43 +0300

    HW-8 initial commit

---
+++ HW-8/src/main/java/ru/hse/java/kostya/ScorchedEarthGame.java
@@ -0,0 +1,16 @@
+package ru.hse.java.kostya;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ScorchedEarthGame extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
